0fe13cc0c214d54eb742693cc43df8515c4b002d
Krlosr79 <krlosr79@hotmail.com>
2020-03-13 14:45:50 +0000

    initial

---
+++ BlazorTest/Data/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTest.Data
+{
+    public class TodoItem
+    {
+        public string title { get; set; }
+        public Boolean isDone { get; set; }
+    }
+}
---
+++ Conjugador-Nuevo/Data/LematizadorResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conjugador_Nuevo.Data
+{
+    public class LematizadorResponse
+    {
+        private string prefijo;
+        private int claveCanonica;
+        private int codigoFlexion;
+        private int frecuencia;
+        private string infinitivo;
+        private string pronombreenclitico;
+
+        public LematizadorResponse(string prefijo, int claveCanonica, int codigoFlexion, int frecuencia, string infinitivo, string pronombreenclitico)
+        {
+            this.Prefijo = prefijo;
+            this.ClaveCanonica = claveCanonica;
+            this.CodigoFlexion = codigoFlexion;
+            this.Frecuencia = frecuencia;
+            this.Infinitivo = infinitivo;
+            this.Pronombreenclitico = pronombreenclitico;
+        }
+
+        public string Prefijo { get => prefijo; set => prefijo = value; }
+        public int ClaveCanonica { get => claveCanonica; set => claveCanonica = value; }
+        public int CodigoFlexion { get => codigoFlexion; set => codigoFlexion = value; }
+        public int Frecuencia { get => frecuencia; set => frecuencia = value; }
+        public string Infinitivo { get => infinitivo; set => infinitivo = value; }
+        public string Pronombreenclitico { get => pronombreenclitico; set => pronombreenclitico = value; }
+
+        public override string ToString()
+        {
+            return string.Format("Prefijo: {0} \n clave canónica: {1} \n código de flexión: {2} \n frecuencia: {3} \n infinitivo: {4} \n pronombre enclítico: {5}", prefijo, claveCanonica, codigoFlexion, frecuencia, infinitivo, pronombreenclitico);
+        }
+    }
+}

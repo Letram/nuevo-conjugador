e5b727c31d28df1c0780fe014c8d68d8f9e8916d
Carlos Martel Lamas <carlos.martel105@alu.ulpgc.es>
2020-04-03 20:08:43 +0100

    refactor completo de los tiempos verbales

--- Conjugador-Blazor/Data/ConjugacionVerbo.cs
+++ Conjugador-Blazor/Data/ConjugacionVerbo.cs
@@ -9,9 +9,13 @@ namespace Conjugador_Blazor.Data
     public class ConjugacionVerbo
     {
         public string[] nombreConjugacion { get; set; }
-        public List<InfoConjugacion> conjEsp { get; set; }
-        public List<InfoConjugacion> conjVos { get; set; }
-        public List<InfoConjugacion> conjCan { get; set; }
-        public List<InfoConjugacion> conjFor { get; set; }
+        //public List<InfoConjugacion> conjEsp { get; set; }
+        //public List<InfoConjugacion> conjVos { get; set; }
+        //public List<InfoConjugacion> conjCan { get; set; }
+        //public List<InfoConjugacion> conjFor { get; set; }
+        public List<List<InfoConjugacion>> conjEsp { get; set; }
+        public List<List<InfoConjugacion>> conjVos { get; set; }
+        public List<List<InfoConjugacion>> conjCan { get; set; }
+        public List<List<InfoConjugacion>> conjFor { get; set; }
     }
 }

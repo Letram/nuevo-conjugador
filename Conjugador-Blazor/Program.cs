b45d156c6b30c4f6292af4d2fb5e806a0150f23e
Krlosr79 <krlosr79@hotmail.com>
2020-05-14 13:51:50 +0100

    arreglados un monton de cosas. quedan lo puntos 2,3,4,7, 16, 19, 20, 24, 27, 31. Revisar el punto 1

--- Conjugador-Blazor/Program.cs
+++ Conjugador-Blazor/Program.cs
@@ -2,12 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ConexionConjugador;
+using ConexionCorrector;
+using ConexionLematizador;
 
 namespace Conjugador_Blazor
 {
@@ -16,6 +20,32 @@ namespace Conjugador_Blazor
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            using (var conjugador = new ServicioConjugacionClient(ServicioConjugacionClient.EndpointConfiguration.BasicHttpsBinding_IServicioConjugacion))
+            {
+                using (var lematizador = new ServicioLematizacionClient(ServicioLematizacionClient.EndpointConfiguration.BasicHttpsBinding_IServicioLematizacion))
+                {
+                    using (var corrector = new ServicioSpellerClient(ServicioSpellerClient.EndpointConfiguration.BasicHttpsBinding_IServicioSpeller))
+                    {
+
+                        ThreadPool.QueueUserWorkItem(
+                        new WaitCallback(delegate
+                        {
+                            while (true)
+                            {
+                                Thread.Sleep(TimeSpan.FromSeconds(100));
+                                QueueDummyIOCPWork();
+                            }
+                        }));
+
+                        conjugador.OpenAsync();
+                        lematizador.OpenAsync();
+                        corrector.OpenAsync();
+
+                        Console.WriteLine("Service running...");
+                        Console.ReadKey(false);
+                    }
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -24,5 +54,14 @@ namespace Conjugador_Blazor
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        //solucion de https://stackoverflow.com/questions/33389896/c-sharp-wcf-hosted-in-windows-service-goes-idle-after-5-minutes/33396091#33396091
+        private static unsafe void QueueDummyIOCPWork()
+        {
+            Overlapped ovl = new Overlapped();
+            NativeOverlapped* pOvl = null;
+            pOvl = ovl.Pack((a, b, c) => { Overlapped.Unpack(pOvl); Overlapped.Free(pOvl); }, null);
+            ThreadPool.UnsafeQueueNativeOverlapped(pOvl);
+        }
     }
 }
--- Conjugador-Blazor/wwwroot/js/scripts.js
+++ Conjugador-Blazor/wwwroot/js/scripts.js
@@ -1,5 +1,18 @@
-function toggle(){
-    let toggleBtn = event.target;
-    console.log($.trim($(toggleBtn).text()));
-    $(toggleBtn).text($.trim($(toggleBtn).text()) === "Less" ? "More" : "Less");
+$(document).ready(function () {
+
+});
+
+function pressEnterIfNeeded(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("searchBtn").click();
+    }
+}
+
+function toggle() {
+    let toggleBtn = event.currentTarget;
+    $(toggleBtn).toggleClass("rotated");
 }
